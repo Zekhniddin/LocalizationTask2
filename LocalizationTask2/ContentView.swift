c9f43a3a9af1750fa30fde0902b6678259f26cb0
Zekhniddin <zexjumayev@gmail.com>
2021-01-31 22:22:35 +0500

    Initial Commit

---
+++ LocalizationTask2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalizationTask2
+//
+//  Created by Зехниддин on 31/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
