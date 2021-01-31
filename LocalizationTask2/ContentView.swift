385b8681e3b2381ac1562593ca1070b66000afda
Zekhniddin <zexjumayev@gmail.com>
2021-01-31 22:46:42 +0500

    ...

--- LocalizationTask2/ContentView.swift
+++ LocalizationTask2/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var pdp_Best = "PDP - Best Programming Academy".localized()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Text(pdp_Best)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                
+                HStack(spacing: 0) {
+                    Button(action: {
+                        Bundle.setLanguage(lang: "en")
+                        pdp_Best = "PDP - Best Programming Academy".localized()
+                    }) {
+                        Text("English").frame(maxWidth: .infinity, maxHeight: 70).background(Color.red)
+                    }
+                    Button(action: {
+                        Bundle.setLanguage(lang: "ko")
+                        pdp_Best = "PDP - Best Programming Academy".localized()
+                    }) {
+                        Text("Korean").frame(maxWidth: .infinity, maxHeight: 70).background(Color.blue)
+                    }
+                    Button(action: {
+                        Bundle.setLanguage(lang: "zh")
+                        pdp_Best = "PDP - Best Programming Academy".localized()
+                    }) {
+                        Text("Chinese").frame(maxWidth: .infinity, maxHeight: 70).background(Color.green)
+                    }
+                }.foregroundColor(.white)
+            }.navigationBarTitle("Title".localized(), displayMode: .inline)
+        }
     }
 }
 
